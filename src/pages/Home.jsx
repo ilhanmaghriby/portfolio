a26cfe501f5c68c118a51dbc933f8e56dc2e2045
ilhanmaghriby <ilhan.maghriby23@gmail.com>
2023-09-25 23:48:42 +0700

    remove darkmode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Contact from "../components/contact/Contact";
 import Footer from "../components/footer/Footer";
 import Hero from "../components/hero/Hero";
@@ -9,20 +9,16 @@ import Work from "../components/work/Work";
 import Certificate from "../components/certificate/Certificate";
 
 const Home = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  function toggleDarkMode() {
-    setDarkMode((prevDarkMode) => !prevDarkMode);
-  }
   return (
     <>
-      <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Hero darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Skills darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Work darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Resume darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Certificate darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Contact darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <Footer darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <NavBar />
+      <Hero />
+      <Skills />
+      <Work />
+      <Resume />
+      <Certificate />
+      <Contact />
+      <Footer />
     </>
   );
 };
